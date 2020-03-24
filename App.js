18c53977adf055c4def986a84bd5b283d36df544
mohiyuddinshaikh <mohiyuddin.shaikh46@gmail.com>
2020-03-24 15:54:38 +0530

    Navigation added

--- App.js
+++ App.js
@@ -14,6 +14,7 @@ import {
   View,
   Text,
   StatusBar,
+  Button,
 } from 'react-native';
 
 import {
@@ -23,52 +24,121 @@ import {
   DebugInstructions,
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+
+const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
+const HomeStack = createStackNavigator();
+
+const HomeTabNavigator = () => {
+  return (
+    <Tab.Navigator>
+      {/* <Tab.Screen name="Home" component={HomeScreen} /> */}
+      <Tab.Screen name="Home" component={HomeStackNavigator} />
+      <Tab.Screen name="Saved" component={SavedRecipeScreen} />
+      <Tab.Screen name="Profile" component={ProfileScreen} />
+    </Tab.Navigator>
+  );
+};
+
+const HomeStackNavigator = () => {
+  return (
+    <HomeStack.Navigator>
+      <HomeStack.Screen name="Home" component={HomeScreen} />
+      <HomeStack.Screen
+        name="RecipeDetailsScreen"
+        component={RecipeDetailsScreen}
+      />
+    </HomeStack.Navigator>
+  );
+};
+
+function HomeScreen({navigation}) {
+  return (
+    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+      <Text>Home!</Text>
+      <Button
+        title="Recipe details"
+        onPress={() => navigation.navigate('RecipeDetailsScreen')}
+      />
+    </View>
+  );
+}
+
+function SavedRecipeScreen() {
+  return (
+    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+      <Text>Saved Recipes!</Text>
+    </View>
+  );
+}
+
+function ProfileScreen() {
+  return (
+    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+      <Text>My profile!</Text>
+    </View>
+  );
+}
+
+function RecipeDetailsScreen() {
+  return (
+    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+      <Text>Recipe Details Screen!</Text>
+    </View>
+  );
+}
+
+function shouldHeaderBeShown(route) {
+  const routeName = route.state
+    ? route.state.routes[route.state.index].name
+    : 'Home';
+
+  if (routeName === 'Home') {
+    return false;
+  } else {
+    return true;
+  }
+}
+
+function getHeaderTitle(route) {
+  console.log('route', route);
+  const routeName = route.state
+    ? route.state.routes[route.state.index].name
+    : 'Home';
+  switch (routeName) {
+    case 'Home':
+      return 'Home';
+    case 'Saved':
+      return 'Saved Recipes';
+    case 'Profile':
+      return 'My Profile';
+  }
+}
 
 const App: () => React$Node = () => {
   return (
     <>
-      <Text>Hello from moin</Text>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen
+            name="home"
+            component={HomeTabNavigator}
+            options={({route}) => ({
+              title: getHeaderTitle(route),
+              headerShown: shouldHeaderBeShown(route),
+            })}
+          />
+          <Stack.Screen name="Saved" component={SavedRecipeScreen} />
+          <Stack.Screen name="Profile" component={ProfileScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
     </>
   );
 };
 
-const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
-  },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
-});
+const styles = StyleSheet.create({});
 
 export default App;
